3631f50bac5c801dd02b2d3e42cc02728527bb1b
smhrd <smhrd@211.227.224.240>
2022-03-10 16:40:40 +0900

    당부말씀

--- Testgit/src/main/java/Test03.java
+++ Testgit/src/main/java/Test03.java
@@ -7,6 +7,8 @@ public class Test03 {
 	System.out.println("마지막테스트");
 	
 	System.out.println("심각 졸림");
+	
+	System.out.println("함부러 푸쉬를 하지말자 커밋은 상관없다 니꺼에서만 커밋되는거라 근데 푸쉬해버리면 서버로 날라가서 ㅈ망한다.");
 	}
 
 }
